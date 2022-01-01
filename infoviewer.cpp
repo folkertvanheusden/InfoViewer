993752f141c0d9a9222cc626dabff28fe221fe64
folkert van heusden <mail@vanheusden.com>
2022-01-01 21:36:05 +0100

    mqtt

--- infoviewer.cpp
+++ infoviewer.cpp
@@ -1,3 +1,4 @@
+#include <mosquitto.h>
 #include <mutex>
 #include <thread>
 #include <unistd.h>
@@ -45,34 +46,28 @@ TTF_Font * load_font(const std::string & filename, unsigned int font_height, boo
         return font;
 }
 
-class scroller
+class container
 {
 private:
+	TTF_Font *font { nullptr };
+
+protected:
 	std::mutex lock;
 	SDL_Surface *surface { nullptr };
-	SDL_Rect pos { 0, 0, 0, 0 };
-	TTF_Font *font { nullptr };
-	std::thread *th { nullptr };
 	SDL_Color col { 0, 0, 0, 0 };
 
 public:
-	scroller(const std::string & font_file, const int font_height, const int r, const int g, const int b)
+	container(const std::string & font_file, const int font_height)
 	{
-		col.r = r;
-		col.g = g;
-		col.b = b;
-
 		font = load_font(font_file, font_height, true);
-
-		th = new std::thread(std::ref(*this));
 	}
 
-	~scroller()
+	virtual ~container()
 	{
-		delete th;
+		SDL_FreeSurface(surface);
 	}
 
-	void set_text(const std::string & text)
+	virtual void set_text(const std::string & text)
 	{
 		ttf_lock.lock();
 		SDL_Surface *temp_new = TTF_RenderUTF8_Solid(font, text.c_str(), col);
@@ -81,18 +76,98 @@ public:
 		lock.lock();
 		SDL_Surface *temp_prev = surface;
 		surface = temp_new;
-		pos.w = surface->w,
-		pos.h = surface->h,
 		lock.unlock();
 
 		SDL_FreeSurface(temp_prev);
+	}
+};
+
+class text_box : public container
+{
+private:
+	SDL_Rect pos { 0, 0, 0, 0 };
+
+public:
+	text_box(const std::string & font_file, const int font_height, const int r, const int g, const int b) : container(font_file, font_height)
+	{
+		col.r = r;
+		col.g = g;
+		col.b = b;
+	}
+
+	virtual ~text_box()
+	{
+	}
+
+	void set_text(const std::string & text) override
+	{
+		container::set_text(text);
+
+		lock.lock();
+		pos.w = surface->w;
+		pos.h = surface->h;
+		lock.unlock();
 	}
 
 	void put_scroller(screen_descriptor_t *const sd, const int x, const int y, const int w, const int h)
 	{
 		lock.lock();
 
-		if (surface) {
+		if (surface && pos.w && pos.h) {
+			const int end_x = x * sd->xsteps + w * sd->xsteps;
+
+			SDL_Rect dest { x * sd->xsteps + 1, y * sd->ysteps + 1, sd->xsteps * w - 2, sd->ysteps * h - 2 };
+
+			SDL_Rect pos_work = pos;
+
+			while(dest.x < end_x) {
+				SDL_BlitSurface(surface, &pos_work, sd->screen, &dest);
+				dest.x += pos_work.w - pos_work.x;
+				pos_work.x = 0;
+			}
+		}
+
+		lock.unlock();
+	}
+};
+
+class scroller : public container
+{
+private:
+	SDL_Rect pos { 0, 0, 0, 0 };
+	std::thread *th { nullptr };
+	std::string text_seperator;
+
+public:
+	scroller(const std::string & font_file, const int font_height, const int r, const int g, const int b, const std::string & text_seperator) : container(font_file, font_height), text_seperator(text_seperator)
+	{
+		col.r = r;
+		col.g = g;
+		col.b = b;
+
+		th = new std::thread(std::ref(*this));
+	}
+
+	virtual ~scroller()
+	{
+		delete th;
+	}
+
+	void set_text(const std::string & text) override
+	{
+		container::set_text(text + text_seperator);
+
+		lock.lock();
+		pos.w = surface->w;
+		pos.h = surface->h;
+		lock.unlock();
+	}
+
+	void put_scroller(screen_descriptor_t *const sd, const int x, const int y, const int w, const int h)
+	{
+		lock.lock();
+
+		if (surface && pos.w && pos.h) {
 			const int end_x = x * sd->xsteps + w * sd->xsteps;
 
 			SDL_Rect dest { x * sd->xsteps + 1, y * sd->ysteps + 1, sd->xsteps * w - 2, sd->ysteps * h - 2 };
@@ -113,11 +188,55 @@ public:
 		for(;;) {
 			usleep(10000);
 
-			lock.lock();
-			pos.x++;
-			pos.x %= pos.w;
-			lock.unlock();
+			if (pos.w) {
+				lock.lock();
+
+				pos.x++;
+				pos.x %= pos.w;
+
+				lock.unlock();
+			}
 		}
+	}
+};
+
+void on_message(struct mosquitto *, void *arg, const struct mosquitto_message *msg, const mosquitto_property *)
+{
+	container *c = (container *)arg;
+
+	std::string new_text((const char *)msg->payload, msg->payloadlen);
+
+	c->set_text(new_text);
+}
+
+class mqtt_feed
+{
+private:
+	std::thread *th { nullptr };
+	struct mosquitto *mi { nullptr };
+
+public:
+	mqtt_feed(const std::string & host, const int port, const std::string & topic, container *const c)
+	{
+		mi = mosquitto_new("infoviewer", true, c);
+
+		mosquitto_connect(mi, host.c_str(), port, 30);
+
+		mosquitto_message_v5_callback_set(mi, on_message);
+
+		mosquitto_subscribe(mi, 0, topic.c_str(), 0);
+
+		th = new std::thread(std::ref(*this));
+	}
+
+	virtual ~mqtt_feed()
+	{
+	}
+
+	void operator()()
+	{
+		for(;;)
+			mosquitto_loop(mi, 11000, 1);
 	}
 };
 
@@ -128,19 +247,25 @@ int main(int argc, char *argv[])
 
 	TTF_Init();
 
+	mosquitto_lib_init();
+
 	int n_columns = 80, n_rows = 25;
 	bool grid = true;
+	bool full_screen = false;
+
+	int create_w = 1440, create_h = 800;
 
 	const SDL_VideoInfo *svi = SDL_GetVideoInfo();
-	SDL_Surface *screen = SDL_SetVideoMode(0, 0, 32, (svi->hw_available ? SDL_HWSURFACE : SDL_SWSURFACE) | SDL_ASYNCBLIT);
+	SDL_Surface *screen = SDL_SetVideoMode(create_w, create_h, 32, (svi->hw_available ? SDL_HWSURFACE : SDL_SWSURFACE) | SDL_ASYNCBLIT | (full_screen ? SDL_FULLSCREEN : 0));
 	const int w = screen->w;
 	const int h = screen->h;
 	const int xsteps = w / n_columns;
 	const int ysteps = h / n_rows;
 	screen_descriptor_t sd { screen, w, h, xsteps, ysteps };
 
-	scroller s("/usr/share/vlc/skins2/fonts/FreeSans.ttf", ysteps * 5, 255, 255, 255);
-	s.set_text("Dit is een test.");
+	scroller s("/usr/share/vlc/skins2/fonts/FreeSans.ttf", ysteps * 5, 255, 255, 255, " *** ");
+
+	mqtt_feed mf("mauer", 1883, "dak/keep-alive", &s);
 
 	for(;;) {
 		if (grid) {
